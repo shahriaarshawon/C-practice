17e7d3c8e2d8e7d83991fb8f2b225107923929d1
Shahriar Arafat Shawon <54241282+shahriaarshawon@users.noreply.github.com>
2019-10-05 00:58:44 +0600

    Add files via upload

---
+++ ConsoleAppSampleClass/ConsoleAppSampleClass/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace means folder,directory or packages
+namespace ConsoleApplicationSampleClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student studentOne = new Student(); //studentOne class type variable
+            studentOne.SetId(100);
+            studentOne.SetName("Bruce");
+            studentOne.SetCgpa(3.56);
+
+            studentOne.ShowInformation();
+
+            //Console.WriteLine("{0}",studentOne.GetId());
+
+            Student studentTwo = studentOne;
+            //studentTwo = studentOne;
+            studentTwo = new Student();
+            studentTwo.SetId(200);
+            studentTwo.ShowInformation();
+
+            studentOne.ShowInformation();
+
+            Student studentThree = new Student(200, "Clerk", 3.28);
+            studentThree.ShowInformation();
+
+        }
+    }
+}
---
+++ ConsoleAppSampleClass/ConsoleAppSampleClass/Student.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationSampleClass
+{
+    class Student
+    {
+        private int id;
+        private string name;
+        private double cgpa;
+
+        public int GetId()
+        {
+            return this.id;
+        }
+        //s.id = 100//s.SetId(100)
+        public void SetId(int id)
+        {
+            this.id = id;
+        }
+
+        public string GetName()
+        {
+            return this.name;
+        }
+
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
+
+        public double GetCgpa()
+        {
+            return this.cgpa;
+        }
+
+        public void SetCgpa(double cgpa)
+        {
+            if (cgpa >= 0 && cgpa <= 4.00)
+                this.cgpa = cgpa;
+            else
+                this.cgpa = 0.0;
+
+        }
+
+        public Student()
+        {
+
+        }
+
+        public Student(int id, string name, double cgpa)
+        {
+            this.SetId(id);
+            this.SetName(name);
+            this.SetCgpa(cgpa);
+        }
+
+        public void ShowInformation()
+        {
+            Console.WriteLine("{0}", this.GetId());
+            Console.WriteLine("{0}", this.GetName());
+            Console.WriteLine("{0}\n", this.GetCgpa());
+        }
+
+
+
+        //set method means write,get means read 
+    }
+}
