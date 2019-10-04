e5cb2e818f104e7def1a815ff10ff6a7ba78af21
Shahriar Arafat Shawon <54241282+shahriaarshawon@users.noreply.github.com>
2019-10-05 00:55:28 +0600

    Add files via upload

---
+++ BuiscuitApp/BuiscuitApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuiscuitApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Buiscuit buiscuitOne = new Buiscuit();
+            buiscuitOne.SetName("Cookies");
+            buiscuitOne.SetCompany("Pran");
+            buiscuitOne.SetUnitPrice(10);
+            //buiscuitOne.SetProductionDate(10);
+            //buiscuitOne.SetExpiredate(10);
+            buiscuitOne.ShowInformation();
+
+
+            Buiscuit buiscuitTwo = new Buiscuit("Choco Milk", "Pran", 50);
+            buiscuitTwo.ShowInformation();
+        }
+    }
+}
