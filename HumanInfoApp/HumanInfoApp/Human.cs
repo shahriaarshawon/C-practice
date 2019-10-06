fa3fece5a78cd707fa93b2ede30645120b74f71d
Shahriar Arafat Shawon <54241282+shahriaarshawon@users.noreply.github.com>
2019-10-06 16:33:07 +0600

    Add files via upload

---
+++ HumanInfoApp/HumanInfoApp/Human.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumanInfoApp
+{
+    struct OurDate
+    {
+        public byte date;
+        public byte month;
+        public int year;
+
+        public void PrintDateOfBirth()
+        {
+            Console.Write("Date of Birth: {0}", this.date);
+            Console.Write("/ {0}", this.month);
+            Console.WriteLine("/ {0}", this.year);
+
+        }
+    }
+
+    class Human
+    {
+        private int id;
+        private string name;
+        private OurDate dateOfBirth;
+        private byte bph;
+        private byte bpl;
+        private string gender;
+        private byte heartRate;
+
+        
+        public int GetId()
+        {
+            return this.id;
+        }
+        
+        public void SetId(int id)
+        {
+            this.id = id;
+        }
+
+        public string GetName()
+        {
+            return this.name;
+        }
+
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
+
+        public OurDate GetDateOfBirth()
+        {
+            return this.dateOfBirth;
+        }
+
+        public void SetDateOfBirth(OurDate dateOfBirth)
+        {
+            this.dateOfBirth = dateOfBirth;
+        }
+
+        public byte GetBph()
+        {
+            return this.bph;
+        }
+        
+        public void SetBph(byte bph)
+        {
+            this.bph = bph;
+        }
+
+        public byte GetBpl()
+        {
+            return this.bpl;
+        }
+        
+        public void SetBpl(byte bpl)
+        {
+            this.bpl = bpl;
+        }
+
+        public string GetGender()
+        {
+            return this.gender;
+        }
+
+        public void SetGender(string gender)
+        {
+            this.gender = gender;
+        }
+
+        public byte GetHeartRate()
+        {
+            return this.heartRate;
+        }
+        
+        public void SetHeartRate(byte heartRate)
+        {
+            this.heartRate = heartRate;
+        }
+
+
+
+        
+
+        public Human()
+        {
+
+        }
+
+        public Human(int id, string name, OurDate dateOfBirth, byte bph, byte bpl, string gender, byte heartRate)
+        {
+            this.SetId(id);
+            this.SetName(name);
+            this.SetDateOfBirth(dateOfBirth);
+            this.SetBph(bph);
+            this.SetBpl(bpl);
+            this.SetGender(gender);
+            this.SetHeartRate(heartRate);
+        }
+
+        public void HeartRate(byte heartRate)
+        {
+            if (heartRate >= 70 && heartRate <= 90)
+            {
+                Console.WriteLine("{0}", " healthy ");
+            }
+            else 
+            {
+                Console.WriteLine("{0}", " Unhealthy ");
+            }
+        }
+
+
+        public void ShowInformation()
+        {
+            Console.WriteLine("{0}", this.GetId());
+            Console.WriteLine("{0}", this.GetName());
+            Console.WriteLine("{0}", this.GetDateOfBirth());
+            Console.WriteLine("{0}", this.GetBph());
+            Console.WriteLine("{0}", this.GetBpl());
+            Console.WriteLine("{0}", this.GetGender());
+            Console.WriteLine("{0}\n", this.GetHeartRate());
+        }
+    }
+}
---
+++ HumanInfoApp/HumanInfoApp/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumanInfoApp
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            OurDate dateOfBirth1 = new OurDate();
+            dateOfBirth1.date = 10;
+            dateOfBirth1.month = 02;
+            dateOfBirth1.year = 1990;
+            dateOfBirth1.PrintDateOfBirth();
+
+            OurDate dateOfBirth2 = new OurDate();
+            dateOfBirth2.date = 10;
+            dateOfBirth2.month = 05;
+            dateOfBirth2.year = 1997;
+            dateOfBirth2.PrintDateOfBirth();
+
+            OurDate dateOfBirth3 = new OurDate();
+            dateOfBirth3.date = 18;
+            dateOfBirth3.month = 07;
+            dateOfBirth3.year = 1998;
+            dateOfBirth3.PrintDateOfBirth();
+
+            Human humanOne = new Human();
+            humanOne.SetId(100);
+            humanOne.SetName("Iron Man");
+            humanOne.SetDateOfBirth(dateOfBirth1);
+            humanOne.SetBph(113);
+            humanOne.SetBpl(85);
+            humanOne.SetGender("Male");
+            humanOne.SetHeartRate(80);
+
+            humanOne.ShowInformation();
+
+
+
+            Human humanTwo = new Human(200, "thor", dateOfBirth2, 90, 50, "Male", 60);
+            humanTwo.ShowInformation();
+
+            Human humanThree = new Human(200, "Wonder", dateOfBirth2, 90, 50, "Female", 60);
+            humanThree.ShowInformation();
+
+            Human humanFour = new Human(200, "Wonder", dateOfBirth3, 90, 50, "Female", 60);
+            humanFour.ShowInformation();
+        }
+    }
+}
